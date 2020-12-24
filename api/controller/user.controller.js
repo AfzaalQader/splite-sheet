bee6dfd0a63cfd6c8d4cbf07a6eccad0b947357e
Muhammad Afzaal <38922111+AfzaalQader@users.noreply.github.com>
2020-12-24 13:01:46 -0800

    chaning

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,9 +1,14 @@
 const UserModel = require('../../models/user.model');
+const success_response = require('../../public/javascripts/response/success.response.json');
+var success_200 = success_response.success_200;
+
 const controller = {}
 
 controller.getAllUsers = async (req, res) => {
     const resData = await UserModel.findAll();
-    res.send(resData);
+    success_200.message = "Get all users ";
+    success_200.data.items = resData;
+    res.send(success_200);
 }
 
 controller.getUser =async (req, res) => {
@@ -13,14 +18,17 @@ controller.getUser =async (req, res) => {
             id: id
         }
     });
-    res.send(resData);
+    success_200.message = "get single user data";
+    success_200.data.items = resData;
+    res.send(success_200);
 }
 
 controller.createUser =async (req, res) => {
     console.log("Create method called");
     const resData = await UserModel.create(req.body);
-    console.log(resData);
-    res.send(resData);
+    success_200.message = "New user create successfully";
+    success_200.data.items = resData;
+    res.send(success_200);
 }
 
 module.exports = controller;
